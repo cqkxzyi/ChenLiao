beb751782799e699bc44ec3919ce06f4d8e04020
zhangyi <kxyi-lover@163.com>
2020-05-24 16:11:05 +0800

    jia

---
+++ 源码/ZY.Web.WeiXinWeb/App_Start/CYPayFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CY.WEIXIN.Web.App_Start
+{
+    public class CYPayFilterAttribute : ActionFilterAttribute
+    {
+       
+
+        /// <summary>
+        /// 在Action方法之前调用
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var cyRedirct = filterContext.HttpContext.Request.Path;
+
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
